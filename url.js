5db0101bf3b01d6099dfac9c86bfca366aa57ec4
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-08-19 21:14:38 -0700

    Create url.js
    Maybe?

---
+++ url.js
@@ -0,0 +1,13 @@
+    var res = this.getOrigin(a) === this.getOrigin(b);
+    debug('same', a, b, res);
+    return res;
+  }
+
+, isSchemeEqual: function(a, b) {
+    return (a.split(':')[0] === b.split(':')[0]);
+  }
+
+, addPath: function (url, path) {
+    var qs = url.split('?');
+    return qs[0] + path + (qs[1] ? '?' + qs[1] : '');
+  }
